4a386dc76cba87f02b80dc330c453f0c432e5edb
xlcbingo1999 <1220368815@qq.com>
2023-06-21 15:21:04 +0800

    feature: cr绘图

--- generate_plots.py
+++ generate_plots.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import re
 import os
+import itertools
+import math
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from textwrap import fill
@@ -45,7 +47,94 @@ def from_y_label_name_2_add_columns_keys_2_need_max_map(y_label_name_arr):
             add_columns_keys_2_need_max_map[y_label_name] = True
     return add_columns_keys_2_need_max_map
 
-def draw_group_plot(target_pic_name, keys_str, env_policy_groups, env_x_groups, 
+def draw_plot_worker(fill_between_flag, results, results_min, results_max,
+                    env_policy_groups, env_x_groups, 
+                    y_label_name, env_x_label, get_policy_map_func,
+                    params,
+                    current_dir, target_pic_name):
+    max_one_line_length = params["max_one_line_length"]
+    font_size = params["font_size"]
+    line_width = params["line_width"]
+    fill_between_alpha = params["fill_between_alpha"]
+    bbox_to_anchor = params["bbox_to_anchor"]
+    labels_pacing = params["label_spacing"]
+    column_spacing = params["column_spacing"]
+    ncol = params["ncol"]
+    marker_size = params["marker_size"]
+    same_distance = params["same_distance"]
+    figsize = params["figsize"]
+    
+    colors, _, markers = get_mark_color_hatch_marker()
+
+    fig = plt.figure(figsize=figsize)
+    plt.grid(linestyle="--", axis='y', alpha=0.5)  # 设置背景网格线为虚线
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)  # 去掉上边框
+    ax.spines['right'].set_visible(False)  # 去掉右边框
+
+    for policy_index, policy in enumerate(env_policy_groups):
+        print(f"policy_index: {policy_index}: policy: {policy}, label: {get_policy_map_func(policy)}")
+        print(f"results[group_index]: {results[policy_index]}")
+        if same_distance:
+            env_x_groups_str = range(len(env_x_groups))
+        else:
+            env_x_groups_str = env_x_groups
+        plt.plot(
+            env_x_groups_str, 
+            results[policy_index], 
+            marker=markers[policy_index], 
+            markersize=marker_size,
+            color=colors[policy_index], 
+            label=get_policy_map_func(policy), 
+            linewidth=line_width
+        )
+        if fill_between_flag:
+            plt.fill_between(
+                env_x_groups_str, 
+                results_min[policy_index], 
+                results_max[policy_index], 
+                color=colors[policy_index], 
+                alpha=fill_between_alpha
+            )
+
+    group_labels = list(str(hen) for hen in env_x_groups)  # x轴刻度的标识
+    plt.xticks(
+        env_x_groups_str, 
+        group_labels, 
+        fontsize=font_size, 
+        fontweight='bold'
+    )
+    plt.yticks(fontsize=font_size, fontweight='bold')
+    plt.xlabel(env_x_label, fontsize=font_size, fontweight='bold')
+    plt.ylabel(fill(y_label_name, max_one_line_length), fontsize=font_size, fontweight='bold')
+    if np.mean(results) < 1e-2 or np.mean(results) > 1e2:
+        plt.ticklabel_format(style='sci',scilimits=(0,0),axis='y')
+
+    # plt.xlim(0.9, 6.1)  # 设置x轴的范围
+    # plt.ylim(1.5, 16)
+
+    plt.legend()          #显示各曲线的图例
+    leg = plt.gca().get_legend()
+    ltext = leg.get_texts()
+    plt.setp(ltext, fontsize=font_size, fontweight='bold')  # 设置图例字体的大小和粗细
+    # plt.legend(loc=4, bbox_to_anchor=(0.98,1.0),borderaxespad = 0.)
+    legend_properties = {
+        'weight':'bold',
+        'size': font_size-2
+    }
+    plt.legend(bbox_to_anchor=bbox_to_anchor, labelspacing=labels_pacing, columnspacing=column_spacing, loc='upper center', ncol=ncol, prop=legend_properties, frameon=False)
+
+    # plt.subplots_adjust(left=0.1, right=0.88)
+
+    plt.tight_layout()
+    result_path_prefix = os.path.join(current_dir, f"{target_pic_name}_{y_label_name}")
+    plt.savefig(result_path_prefix + '.png', format='png')  # 建议保存为svg格式,再用inkscape转为矢量图emf后插入word中
+    plt.show()
+    pp = PdfPages(result_path_prefix + '.pdf')
+    pp.savefig(fig)
+    pp.close()
+
+def get_result_and_draw_group_plot(target_pic_name, keys_str, env_policy_groups, env_x_groups, 
                 y_label_name_arr, env_x_label, params, fill_between_flag, get_policy_map_func):
     current_dir = "/home/netlab/DL_lab/opacus_testbed/plots"
     path = os.path.join(current_dir, f"{target_pic_name}.csv")
@@ -60,90 +149,17 @@ def draw_group_plot(target_pic_name, keys_str, env_policy_groups, env_x_groups,
     print("---- df.info ----")
     df_with_key.info()
 
-    max_one_line_length = params["max_one_line_length"]
-    font_size = params["font_size"]
-    line_width = params["line_width"]
-    fill_between_alpha = params["fill_between_alpha"]
-    bbox_to_anchor = params["bbox_to_anchor"]
-    labels_pacing = params["label_spacing"]
-    column_spacing = params["column_spacing"]
-    ncol = params["ncol"]
-    marker_size = params["marker_size"]
-    same_distance = params["same_distance"]
     
-    colors, _, markers = get_mark_color_hatch_marker()
     for y_label_name in y_label_name_arr:
         results, results_min, results_max = get_result_avg_min_max_for_y_label_name(
             df_with_key, env_policy_groups, env_x_groups, y_label_name
         )
-
-        fig = plt.figure(figsize=(10, 5))
-        plt.grid(linestyle="--", axis='y', alpha=0.5)  # 设置背景网格线为虚线
-        ax = plt.gca()
-        ax.spines['top'].set_visible(False)  # 去掉上边框
-        ax.spines['right'].set_visible(False)  # 去掉右边框
-
-        for policy_index, policy in enumerate(env_policy_groups):
-            print(f"policy_index: {policy_index}: policy: {policy}, label: {get_policy_map_func(policy)}")
-            print(f"results[group_index]: {results[policy_index]}")
-            if same_distance:
-                env_x_groups_str = range(len(env_x_groups))
-            else:
-                env_x_groups_str = env_x_groups
-            plt.plot(
-                env_x_groups_str, 
-                results[policy_index], 
-                marker=markers[policy_index], 
-                markersize=marker_size,
-                color=colors[policy_index], 
-                label=get_policy_map_func(policy), 
-                linewidth=line_width
-            )
-            if fill_between_flag:
-                plt.fill_between(
-                    env_x_groups_str, 
-                    results_min[policy_index], 
-                    results_max[policy_index], 
-                    color=colors[policy_index], 
-                    alpha=fill_between_alpha
-                )
-
-        group_labels = list(str(hen) for hen in env_x_groups)  # x轴刻度的标识
-        plt.xticks(
-            env_x_groups_str, 
-            group_labels, 
-            fontsize=font_size, 
-            fontweight='bold'
-        )
-        plt.yticks(fontsize=font_size, fontweight='bold')
-        plt.xlabel(env_x_label, fontsize=font_size, fontweight='bold')
-        plt.ylabel(fill(y_label_name, max_one_line_length), fontsize=font_size, fontweight='bold')
-        if np.mean(results) < 1e-2 or np.mean(results) > 1e2:
-            plt.ticklabel_format(style='sci',scilimits=(0,0),axis='y')
-
-        # plt.xlim(0.9, 6.1)  # 设置x轴的范围
-        # plt.ylim(1.5, 16)
-
-        plt.legend()          #显示各曲线的图例
-        leg = plt.gca().get_legend()
-        ltext = leg.get_texts()
-        plt.setp(ltext, fontsize=font_size, fontweight='bold')  # 设置图例字体的大小和粗细
-        # plt.legend(loc=4, bbox_to_anchor=(0.98,1.0),borderaxespad = 0.)
-        legend_properties = {
-            'weight':'bold',
-            'size': font_size-2
-        }
-        plt.legend(bbox_to_anchor=bbox_to_anchor, labelspacing=labels_pacing, columnspacing=column_spacing, loc='upper center', ncol=ncol, prop=legend_properties, frameon=False)
-
-        # plt.subplots_adjust(left=0.1, right=0.88)
-
-        plt.tight_layout()
-        result_path_prefix = os.path.join(current_dir, f"{target_pic_name}_{y_label_name}")
-        plt.savefig(result_path_prefix + '.png', format='png')  # 建议保存为svg格式,再用inkscape转为矢量图emf后插入word中
-        plt.show()
-        pp = PdfPages(result_path_prefix + '.pdf')
-        pp.savefig(fig)
-        pp.close()
+        draw_plot_worker(fill_between_flag, results, results_min, results_max,
+                        env_policy_groups, env_x_groups, 
+                        y_label_name, env_x_label, get_policy_map_func, 
+                        params,
+                        current_dir, target_pic_name)
+        
 
 def draw_group_bar(target_pic_name, keys_str, env_policy_groups, env_x_groups,
                 y_label_name_arr, env_x_label, params, get_policy_map_func):
@@ -456,10 +472,11 @@ def draw_fig_5():
         "center_ratio": 2.5,
         "bar_width_ratio": 2,
         "marker_size": 10,
-        "same_distance": True
+        "same_distance": True,
+        "figsize": (8, 6),
     }
     fill_between_flag = True
-    draw_group_plot(target_pic_name, keys_str, env_policy_groups, env_x_groups, 
+    get_result_and_draw_group_plot(target_pic_name, keys_str, env_policy_groups, env_x_groups, 
                 y_label_name_arr, env_x_label, params, fill_between_flag, get_fig_5_policy_map)
 
 def draw_testbed_fig_1():
@@ -507,7 +524,8 @@ def draw_testbed_fig_1():
         "center_ratio": 2.5,
         "bar_width_ratio": 2,
         "marker_size": 10,
-        "same_distance": True
+        "same_distance": True,
+        "figsize": (8, 6),
     }
     y_label_name_arr = [
         "Significance of all jobs", 
@@ -516,7 +534,7 @@ def draw_testbed_fig_1():
         "Ratio of Allocated Datablocks"
     ]
     fill_between_flag = False
-    draw_group_plot(target_pic_name, keys_str, env_policy_groups, env_x_groups, 
+    get_result_and_draw_group_plot(target_pic_name, keys_str, env_policy_groups, env_x_groups, 
                     y_label_name_arr, env_x_label, params, fill_between_flag, get_testbed_fig_1_policy_map)
     
 def draw_testbed_fig_2():
@@ -575,12 +593,65 @@ def draw_testbed_fig_2():
     draw_group_bar(target_pic_name, keys_str, env_policy_groups, env_x_groups, 
                     y_label_name_arr, env_x_label, params, get_testbed_fig_2_policy_map)
 
+def draw_cr():
+    def get_cr_v2(lamb, h_n_ratio):
+        if (1 - lamb) <= 0:
+            return -1000000
+        else:
+            return (2 - lamb - (1 + h_n_ratio) * math.log(1 + 1/h_n_ratio)) / (1 - lamb)
+
+    
+    hs = [i * 5 for i in range(1, 100)]
+    n = 100
+    env_x_groups = [h/n for h in hs] # h/s
+    env_policy_groups = [i / 100 for i in range(0, 100, 10)] # lambda
+    
+    args_product_list = [d for d in itertools.product(env_policy_groups, env_x_groups)]
+    results = []
+    for lamb in env_policy_groups:
+        temp_result = []
+        for h_n_ratio in env_x_groups:
+            res = get_cr_v2(lamb, h_n_ratio)
+            temp_result.append(res)
+        results.append(temp_result)
+
+    
+    params = {
+        "font_size": 20,
+        "line_width": 1.5,
+        "bar_width": 0.23,
+        "fill_between_alpha": 0.5,
+        "max_one_line_length": 20,
+        "bbox_to_anchor": (0.5,1.35),
+        "label_spacing": 0.05,
+        "column_spacing": 0.2,
+        "ncol": 3,
+        "center_ratio": 2.5,
+        "bar_width_ratio": 2,
+        "marker_size": 10,
+        "same_distance": True,
+        "figsize": (8, 6),
+    }
+    y_label_name = "Competition Ratio"
+    env_x_label = r"$\frac{h}{n}$"
+    def get_policy_map_func(origin_policy):
+        return r"\lambda = {}".format(origin_policy)
+    current_dir = "/home/netlab/DL_lab/opacus_testbed/plots"
+    target_pic_name = "cr"
+    draw_plot_worker(False, results, None, None,
+                    env_policy_groups, env_x_groups, 
+                    y_label_name, env_x_label, get_policy_map_func, 
+                    params,
+                    current_dir, target_pic_name)
+    
+
 if __name__ == "__main__":
     # draw_fig_1()
     # draw_fig_2()
     # draw_fig_6()
     # draw_fig_5()
-    draw_testbed_fig_2()
+    # draw_testbed_fig_2()
+    draw_cr()
     
     # "Number of Allocated Jobs", 
     # "Ratio of Allocated Jobs", 
